7e8c8008dcbe33f20c210ffc3b8d8cc2d05acbc1
lukasevicromuald <lukasevicromuald@gmail.com>
2022-06-06 20:18:53 +0300

    konkatinacija/kompozicija/interpoliacija

--- BasicMokymai/BasicMokymai/Program.cs
+++ BasicMokymai/BasicMokymai/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, \n brother World!");
+
+            Console.Write("isvedimas "); 
+            Console.Write("eiluteje "); 
+            Console.Write("vienoje");
+            Console.WriteLine("tekstas" + "vienoje" + "eiluteje"); //konkatinacija
+            Console.WriteLine("{0} {1} {2}", "isvedimas", "vienoje", "eiluteje"); //kompozicija
+            Console.WriteLine($"{"isvedimas"} {"vienoje"} {"eiluteje"}"); //interpoliacija
+
         }
     }
 }
