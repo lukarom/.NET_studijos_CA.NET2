a86a17e3fef422f5108fe10c6449a7bf3759e5db
lukasevicromuald <lukasevicromuald@gmail.com>
2022-06-07 18:35:22 +0300

    Paskaita 3

---
+++ BasicMokymai/Paskaita_3_Komentarai/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Paskaita_3_Komentarai
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Komentarai");
+        }
+    }
+}
