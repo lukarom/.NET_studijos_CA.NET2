60391d918c8d1776f9340cc5b6f6e6b8b0d9b39c
lukasevicromuald <lukasevicromuald@gmail.com>
2022-06-14 20:59:46 +0300

    Paskaita 6 Klases uzduotys

---
+++ BasicMokymai/Paskaita_6_Reliaciniai_Kitamieji_Uzduotys/Program.cs
@@ -0,0 +1,89 @@
+﻿
+Console.WriteLine("Hello, Reliaciniai kintamieji uzduotys");
+
+//long skaiciusLong = long.MaxValue;
+//short skaiciusShort = short.MaxValue;
+//var skaiciusRezultatas = (double)skaiciusLong / skaiciusShort;
+//Console.WriteLine($"Daliname didesni skaicius is mazensio: skaiciusLong/skaiciusShort = {skaiciusRezultatas}");
+//Console.WriteLine($"Atemame is rezultato didziausia long skaiciu = {skaiciusRezultatas - skaiciusLong}");
+//Console.WriteLine($" Ir pridedame maksimalu int skaiciu = {skaiciusRezultatas + skaiciusLong + int.MaxValue}");
+
+Console.WriteLine($" ----------------------------------------");
+
+//double didelisDouble = 5060590389;
+
+//int didelisInt = Convert.ToInt32((int)didelisDouble);
+//Console.WriteLine($" didelisInt = {didelisInt}");
+//short didelisShort = Convert.ToInt16((short)didelisDouble);
+//Console.WriteLine($" didelisShort = {didelisShort}");
+//byte didelisByte = Convert.ToByte((byte)didelisDouble);
+//Console.WriteLine($" didelisByte = {didelisByte}");
+
+//Console.WriteLine($" ----------------------------------------");
+//Console.WriteLine($" Iveskite rutulio diametra: ");
+
+//var rutulioDiametras = Console.ReadLine();
+//Console.WriteLine($" Rutulio diametras: {rutulioDiametras}");
+
+//Console.WriteLine($" Rutulio Turis: {4 / 3 * 3.14f * (Convert.ToInt32(rutulioDiametras)* Convert.ToInt32(rutulioDiametras)* Convert.ToInt32(rutulioDiametras)/2)}");
+//Console.WriteLine($" Rutulio Plotas: {4 * 3.14f * (Convert.ToInt32(rutulioDiametras)* Convert.ToInt32(rutulioDiametras)/2)}");
+
+Console.WriteLine($" ----------------------------------------");
+
+//Console.WriteLine(" Iveskite atstuma metrais: ");
+//var atstumasMetrais = Console.ReadLine();
+//Console.WriteLine(" Iveskite atstuma metrais: ");
+//var laikasSekundemis = Console.ReadLine();
+
+//var greitisMs = int.Parse(atstumasMetrais) / int.Parse(laikasSekundemis);
+//Console.WriteLine($" Greitis km/h: {(double)greitisMs*3600/1000}");
+//Console.WriteLine($" Greitis km/s: {((double)greitisMs/1000)}");
+
+//Console.WriteLine($" ----------------------------------------");
+
+//Console.WriteLine(" Iveskite x: ");
+//var x = int.Parse(Console.ReadLine());
+//Console.WriteLine(" Iveskite y: ");
+//var y = int.Parse(Console.ReadLine());
+
+//Console.WriteLine(" Funkcija y+2y+x+1");
+//Console.WriteLine($" Funkcijos rezultatas = {y + 2*x + x + 1}");
+
+//Console.WriteLine($" ----------------------------------------");
+
+//Console.WriteLine(" Funkcija y^2+x/2");
+//Console.WriteLine($" Funkcijos rezultatas = {y* y + 2*(double)x/2}");
+
+//var d = Console.ReadLine();
+//Console.WriteLine($"ar null {d == null}");
+//Console.WriteLine($"ar empty {d == string.Empty}");
+
+//Console.WriteLine($" Iveskite penkiazenkli sveika skaiciu: ");
+//var penkSkaicius = Convert.ToDouble(Console.ReadLine().Replace("1", "0"));
+//var rezultatas = penkSkaicius / 2;
+//Console.WriteLine($" Rezultatas: {rezultatas} ");
+
+//Console.WriteLine($" Iveskite penkiazenkli sveika skaiciu: ");
+//var penkSkaicius = Convert.ToDouble(Console.ReadLine()
+//    .Replace("2", "0")
+//    .Replace("3", "0")
+//    .Replace("4", "0")
+//    .Replace("5", "0")
+//    .Replace("6", "0")
+//    .Replace("7", "0")
+//    .Replace("8", "0")
+//    .Replace("9", "0"));
+//var rezultatas = penkSkaicius / 2;
+//Console.WriteLine($" Rezultatas: {rezultatas} ");
+
+
+Console.WriteLine($" Iveskite sveika skaiciu: ");
+var skaicius = int.Parse(Console.ReadLine());
+Console.WriteLine($" {++skaicius} {++skaicius} {++skaicius} {++skaicius} {++skaicius} ");
+
+
+
+
+
+
+
